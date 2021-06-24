298af3efacb9a01c0e2c380572e8d3aa0686e037
Gokul <85551261+aryagoogly@users.noreply.github.com>
2021-06-24 23:18:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-(function() {
-    const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
-
-    let birthday = "June 25, 2021 00:00:00",
-        countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {
-
-            let now = new Date().getTime(),
-                distance = countDown - now;
-
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-            //do something later when date is reached
-            if (distance < 0) {
-                let headline = document.getElementById("headline"),
-                    countdown = document.getElementById("countdown"),
-                    content = document.getElementById("content");
-
-                headline.innerText = "It's your  birthday!";
-                countdown.style.display = "none";
-                content.style.display = "block";
-
-                clearInterval(x);
-            }
-            //seconds
-        }, 0)
-}());
+// Set the date we're counting down to
+var endDate = new Date("June 25, 2021 00:00:00").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
+    // Get todays date and time
+    var now = new Date().getTime();
+    // Find the distance between now an the count down date
+    var distance = endDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Output the result in elements with id="cdHour/cdMin/cdSec"
+    document.getElementById("days").innerHTML = ("0" + days).slice(-2);  
+    document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+    document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+    document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("headline").innerHTML = "Happy Birthday Matti";
+    }
+}, 1000);
